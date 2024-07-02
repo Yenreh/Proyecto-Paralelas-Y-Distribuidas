01cd3488f3df9901c2438040f0a690bb2b52d3b0
yenreh <qherney@gmail.com>
2024-07-02 10:37:42 -0500

    General Update

--- src/pruebas/run_test.sh
+++ src/pruebas/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ejecutar pruebas de Locust para backend
-locust -f backend.py --headless -u 10 -r 1 --csv=results/locust_backend_stats --stop-timeout 60
+locust -f backend.py --headless -u 100 -r 2 --csv=results/locust_backend_stats --stop-timeout 300
 
 # Ejecutar pruebas de Locust para frontend
-#locust -f frontend.py --headless -u 10 -r 1 --csv=results/locust_frontend_stats
+locust -f frontend.py --headless -u 100 -r 2 --csv=results/locust_frontend_stats --stop-timeout 300
