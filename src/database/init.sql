9c6f749088e6a33fd11e1f7da583fa84eaa4877a
yenreh <qherney@gmail.com>
2024-07-02 00:40:30 -0500

    General Update

--- src/database/init.sql
+++ src/database/init.sql
@@ -1,15 +1,13 @@
 CREATE DATABASE IF NOT EXISTS administrator_db;
 
-USE administrator_db;
-
-CREATE TABLE project (
+CREATE TABLE administrator_db.project (
                          id INT AUTO_INCREMENT PRIMARY KEY,
                          name VARCHAR(100) NOT NULL,
                          created_at DATETIME NOT NULL,
                          updated_at DATETIME NOT NULL
 );
 
-CREATE TABLE task (
+CREATE TABLE administrator_db.task (
                       id INT AUTO_INCREMENT PRIMARY KEY,
                       project_id INT NOT NULL,
                       name VARCHAR(100) NOT NULL,
@@ -20,13 +18,13 @@ CREATE TABLE task (
 
 
 -- Insertar datos de prueba en la tabla project
-INSERT INTO project (name, created_at, updated_at) VALUES
+INSERT INTO administrator_db.project (name, created_at, updated_at) VALUES
                                                        ('Project Alpha', '2024-06-01 10:00:00', '2024-06-01 10:00:00'),
                                                        ('Project Beta', '2024-06-02 11:00:00', '2024-06-02 11:00:00'),
                                                        ('Project Gamma', '2024-06-03 12:00:00', '2024-06-03 12:00:00');
 
 -- Insertar datos de prueba en la tabla task
-INSERT INTO task (project_id, name, created_at, updated_at) VALUES
+INSERT INTO administrator_db.task (project_id, name, created_at, updated_at) VALUES
                                                                 (1, 'Task 1 for Alpha', '2024-06-01 11:00:00', '2024-06-01 11:00:00'),
                                                                 (1, 'Task 2 for Alpha', '2024-06-01 12:00:00', '2024-06-01 12:00:00'),
                                                                 (2, 'Task 1 for Beta', '2024-06-02 12:00:00', '2024-06-02 12:00:00'),
