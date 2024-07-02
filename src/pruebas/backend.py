08f5e1d849c5e70fbe5684c51e27ec27fff0bf7d
yenreh <qherney@gmail.com>
2024-07-02 10:36:45 -0500

    General Update

--- src/pruebas/backend.py
+++ src/pruebas/backend.py
@@ -14,23 +14,19 @@ class User(HttpUser):
 
     @task
     def get_projects(self):
-        response = self.client.get("/projects")
-        response.raise_for_status()
+        self.client.get("/projects")
 
     @task
     def get_project(self):
-        response = self.client.get("/projects/1")
-        response.raise_for_status()
+        self.client.get("/projects/1")
 
     @task
     def create_project(self):
-        response = self.client.post("/projects/create", json={"name": "New Project"})
-        response.raise_for_status()
+        self.client.post("/projects/create", json={"name": "New Project"})
 
     @task
     def update_project(self):
-        response = self.client.put("/projects/update/1", json={"name": "Updated Project 1"})
-        response.raise_for_status()
+        self.client.put("/projects/update/1", json={"name": "Updated Project 1"})
 
     # Test for register that does not exist after first run
     @task
@@ -40,37 +36,30 @@ class User(HttpUser):
 
     @task
     def get_project_tasks(self):
-        response = self.client.get("/projects/1/tasks")
-        response.raise_for_status()
+        self.client.get("/projects/1/tasks")
 
     @task
     def get_tasks(self):
-        response = self.client.get("/tasks")
-        response.raise_for_status()
+        self.client.get("/tasks")
 
     @task
     def get_task(self):
-        response = self.client.get("/tasks/1")
-        response.raise_for_status()
+        self.client.get("/tasks/1")
 
     @task
     def create_task(self):
-        response = self.client.post("/tasks/create", json={"name": "New Task for Project 1", "project_id": 1})
-        response.raise_for_status()
+        self.client.post("/tasks/create", json={"name": "New Task for Project 1", "project_id": 1})
 
     # Test with missing required field
     @task
     def create_task2(self):
-        response = self.client.post("/tasks/create", json={"name": "New Task for Project 1"})
-        response.raise_for_status()
+        self.client.post("/tasks/create", json={"name": "New Task for Project 1"})
 
     @task
     def update_task(self):
-        response = self.client.put("/tasks/update/1", json={"name": "Updated Task 1"})
-        response.raise_for_status()
+        self.client.put("/tasks/update/1", json={"name": "Updated Task 1"})
 
     # Test for register that does not exist after first run
     @task
     def delete_task(self):
-        response = self.client.delete("/tasks/delete/2")
-        response.raise_for_status()
+        self.client.delete("/tasks/delete/2")
--- src/pruebas/run_test.sh
+++ src/pruebas/run_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ejecutar pruebas de Locust para backend
-locust -f backend.py --headless -u 10 -r 1 --csv=results/locust_backend_stats
+locust -f backend.py --headless -u 10 -r 1 --csv=results/locust_backend_stats --stop-timeout 60
 
 # Ejecutar pruebas de Locust para frontend
 #locust -f frontend.py --headless -u 10 -r 1 --csv=results/locust_frontend_stats
