078de1aa1bf8c321bb3f5940efeb135a4e7ece7a
yenreh <qherney@gmail.com>
2024-07-06 18:41:41 -0500

    General Update Add delay to deploy frontend

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Navigate to src directory
 cd src/swarm
-docker stack deploy -c backend-deployment.yaml backend_stack
-docker stack deploy -c frontend-deployment.yaml frontend_stack
 docker stack deploy -c database-deployment.yaml database_stack
+docker stack deploy -c backend-deployment.yaml backend_stack
+sleep 10
+docker stack deploy -c frontend-deployment.yaml frontend_stack
